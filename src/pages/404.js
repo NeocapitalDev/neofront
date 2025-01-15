52cba421673f6b36872610c79871087c2101becf
NeocapitalDev <neocapital2025@gmail.com>
2025-01-14 19:41:31 -0500

    page 404 moificada

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,17 +5,17 @@ export default function Error404() {
     return (
         <>
             <Head>
-                <title>404 - Wazend</title>
+                <title>404 - {process.env.NEXT_PUBLIC_NAME_APP}</title>
             </Head>
             <div className="h-screen">
-                <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+                <main className="grid min-h-full place-items-center bg-zinc-900 px-6 py-24 sm:py-32 lg:px-8">
                     <div className="text-center">
-                        <p className="text-base font-bold text-emerald-600">404</p>
-                        <h1 className="mt-4 text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Página no encontrada</h1>
-                        <p className="mt-6 text-base leading-7 text-gray-600">La página que estás buscando no existe o ha sido movida</p>
+                        <p className="text-base font-bold text-amber-600">404</p>
+                        <h1 className="mt-4 text-3xl font-semibold tracking-tight text-gray-300 sm:text-5xl">Página no encontrada</h1>
+                        <p className="mt-6 text-base leading-7 text-gray-300">La página que estás buscando no existe o ha sido movida</p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                         <Link href="/"
-                            className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+                            className="rounded-md bg-amber-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-amber-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500"
                             >
                                     Volver al inicio
                         </Link>
