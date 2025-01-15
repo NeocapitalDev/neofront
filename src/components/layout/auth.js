1f50a2817e2e6611e4e3a82ffd6c6cf035be0799
NeocapitalDev <neocapital2025@gmail.com>
2025-01-14 19:59:32 -0500

    animaciion moneda

--- src/components/layout/auth.js
+++ src/components/layout/auth.js
@@ -8,42 +8,65 @@ import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
-      <div className="h-screen bg-zinc-900">
-        <div className="flex min-h-full flex-1">
+    <div className="h-screen bg-zinc-900">
+      <div className="flex min-h-full flex-1">
 
 
-        <div className="relative hidden w-1/2 lg:block ">
-            <Image
-              className="absolute inset-0 h-full w-full object-cover "
-              src="/images/bg-register.webp"
-              alt="Login Background"
-              width={960} // Ajusta el ancho deseado
-              height={1080} // Ajusta la altura deseada
-            />
-          </div>
+        {/* <div className="relative hidden w-1/2 lg:block ">
+          <Image
+            className="absolute inset-0 h-full w-full object-cover "
+            src="/images/bg-register.jpg"
+            alt="Login Background"
+            width={960} // Ajusta el ancho deseado
+            height={1080} // Ajusta la altura deseada
+          />
+        </div> */}
+
+<div className="relative hidden w-1/2 lg:block">
+  {/* Imagen de fondo */}
+  <Image
+    className="absolute inset-0 h-full w-full object-cover"
+    src="/images/bg-register.jpg"
+    alt="Login Background"
+    width={960}
+    height={1080}
+  />
+
+  {/* Imagen superpuesta */}
+  <Image
+    className="absolute inset-0 m-auto w-[70%] h-auto object-contain"
+    src="/images/moneda.webp"
+    alt="Moneda"
+    width={960}
+    height={1080}
+
+    layout="intrinsic" // Asegura que la relación de aspecto se mantenga
+  />
+</div>
+
 
 
         <div className="flex flex-1 w-1/2 flex-col justify-center px-8 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-            <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
 
-              <Link href="/">
-                <Image
-                  className="h-8 w-auto"
-                  src="/images/logo-dark.png"
-                  alt="Logo"
-                  width={236} // Ajusta el ancho deseado
-                  height={60} // Ajusta la altura deseada
-                />
-              </Link>
+            <Link href="/">
+              <Image
+                className="h-8 w-auto"
+                src="/images/logo-dark.png"
+                alt="Logo"
+                width={236} // Ajusta el ancho deseado
+                height={60} // Ajusta la altura deseada
+              />
+            </Link>
 
-              <main>{children}</main>
-            </div>
+            <main>{children}</main>
           </div>
-
-       
-
         </div>
+
+
+
       </div>
+    </div>
   );
 };
 
