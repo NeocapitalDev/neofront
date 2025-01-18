3c0db7ac5c03203c2f52c04882865054c207e2bf
NeocapitalDev <neocapital2025@gmail.com>
2025-01-18 16:47:23 -0500

    consumir api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost'], // Agrega aquí el dominio de las imágenes
+  },
 };
 
 export default nextConfig;
--- src/pages/support/index.js
+++ src/pages/support/index.js
@@ -1,22 +1,39 @@
 import React from "react";
 import Layout from "../../components/layout/dashboard";
 import Image from "next/image";
+import { useStrapiData } from "../../lib/strapiService";
+import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
-const plataformas = [
-    {
-        nombre: "WhatsApp",
-        url: "#",
-        accion: "Enviar mensaje",
-        icono: "whatsapp.svg",
-    },
-    {
-        nombre: "Discord",
-        url: "#",
-        accion: "Unirse",
-        icono: "discord.svg",
-    },
-];
 const Billing = () => {
+    const { data: supports, error, isLoading } = useStrapiData("supports?populate=*");
+    console.log(supports);
+    
+    const constructImageUrl = (url) => {
+        const baseUrl = process.env.NEXT_PUBLIC_STRAPI_BASE_URL || "http://localhost:1337";
+        console.log(`${baseUrl}${url}`)
+        return `${baseUrl}${url}`;
+    };
+
+    if (isLoading) {
+        return (
+            <Layout title="Billing">
+                <div className="p-6 bg-white shadow-md rounded-lg">
+                    <p className="text-gray-600">Cargando plataformas de soporte...</p>
+                </div>
+            </Layout>
+        );
+    }
+
+    if (error) {
+        return (
+            <Layout title="Billing">
+                <div className="p-6 bg-white shadow-md rounded-lg">
+                    <p className="text-red-600">Error al cargar las plataformas de soporte.</p>
+                </div>
+            </Layout>
+        );
+    }
+
     return (
         <Layout title="Billing">
             <div className="p-6 bg-white shadow-md rounded-lg">
@@ -27,14 +44,16 @@ const Billing = () => {
                             width={30}
                             height={30}
                             className="w-[30px] h-[30px]"
+                            alt="Icono de Soporte"
                         />
-                    </span> Support
+                    </span>
+                    Support
                 </h2>
                 <p className="text-gray-600 mb-6">
                     Si necesitas ayuda, no dudes en contactarnos a través de nuestras principales plataformas. Estamos aquí para responder tus preguntas y brindarte el soporte que necesitas. ¡Conéctate con nosotros ahora!
                 </p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                    {plataformas.map((plataforma, index) => (
+                    {supports.map((plataforma, index) => (
                         <div
                             key={index}
                             className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg transition flex flex-col items-center"
@@ -44,7 +63,7 @@ const Billing = () => {
                                 {/* Icono */}
                                 <div className="flex-shrink-0 bg-gray-100 p-3 rounded-full flex items-center justify-center">
                                     <Image
-                                        src={`/images/support/${plataforma.icono}`}
+                                        src={constructImageUrl(plataforma.icon.url)}
                                         alt={plataforma.nombre}
                                         width={51}
                                         height={51}
@@ -69,14 +88,7 @@ const Billing = () => {
                                 className="inline-flex items-center justify-center bg-amber-500 text-black font-medium py-2 px-4 rounded-lg hover:bg-amber-600 transition w-full"
                             >
                                 {plataforma.accion}
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="ml-2 w-3 h-3"
-                                    viewBox="0 0 512 512"
-                                    fill="#000000"
-                                >
-                                    <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"></path>
-                                </svg>
+                                <ArrowLongRightIcon className="ml-2 w-6 h-6 text-black" />
                             </a>
                         </div>
                     ))}
