1778d78f5989cf90ff01d09e798c90044b406bcb
NeocapitalDev <neocapital2025@gmail.com>
2025-01-21 16:26:54 -0500

    cmabios billing

--- src/pages/billing/index.js
+++ src/pages/billing/index.js
@@ -39,68 +39,65 @@ const Billing = () => {
     return (
         <Layout title="Billing">
             <div className="p-6 dark:bg-zinc-800 border-gray-200  border-2  bg-white shadow-md rounded-lg dark:text-white dark:border-zinc-800 dark:shadow-black">
-            <div className="flex p-5 justify-between items-center mb-4">
-                    <div className="flex items-center space-x-2">
-                        <DocumentTextIcon className="w-6 h-6 text-gray-600" />
+                <div className="flex p-5 justify-between items-center mb-4   border-b-2 dark:border-b">
+                    <div className="flex items-center space-x-2 ">
+                        <DocumentTextIcon className="w-6 h-6 text-gray-600 dark:text-gray-200" />
                         <h1 className="text-xl font-semibold">Facturación</h1>
                     </div>
                     <button className="flex items-center space-x-2 px-4 py-2 bg-gray-100 rounded-lg border border-gray-300 hover:bg-gray-200">
-                        <FunnelIcon className="w-6 h-6 text-gray-600" />
+                        <FunnelIcon className="w-6 h-6 text-white" />
                         <span>Filtro</span>
                     </button>
                 </div>
 
                 <div className="overflow-x-auto rounded-lg shadow-sm">
                     <table className="w-full table-auto text-left border-collapse">
-                        <thead className="border-b">
+                        <thead className="border-b-2 dark:border-b">
                             <tr>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">FTMO Challenge</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Fechas</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Monto a pagar</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Orden</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Cuenta</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Estado</th>
-                                <th className="px-4 py-3 text-zinc-800 font-semibold">Factura y Documentos</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">FTMO Challenge</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Fechas</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Monto a pagar</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Orden</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Cuenta</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Estado</th>
+                                <th className="px-4 py-3 text-zinc-800 dark:text-white font-semibold">Factura y Documentos</th>
                             </tr>
                         </thead>
                         <tbody>
                             {data.length === 0 ? (
                                 <tr>
-    <td colSpan="7" className="px-4 py-6 text-center">
-        <div className="flex flex-col items-center justify-center h-[300px]">
-            <Image 
-                src="/images/billing/icono.svg" 
-                width={300}
-                height={300}
-                className="w-[300px] h-[300px] relative -mt-20"
-                alt="No data icon"
-            />
-            <p className="-mt-20 text-gray-500 relative">No hay órdenes ni transacciones disponibles</p>
-        </div>
-    </td>
-</tr>
-
+                                    <td colSpan="7" className="px-4 py-6 text-center">
+                                        <div className="flex flex-col items-center justify-center h-[300px]">
+                                            <Image
+                                                src="/images/billing/icono.svg"
+                                                width={300}
+                                                height={300}
+                                                className="w-[300px] h-[300px] relative -mt-20"
+                                                alt="No data icon"
+                                            />
+                                            <p className="-mt-20 text-gray-500 relative">No hay órdenes ni transacciones disponibles</p>
+                                        </div>
+                                    </td>
+                                </tr>
                             ) : (
                                 data.map((row, index) => (
-                                    <tr 
-                                        key={index} 
-                                        className={`border-b ${
-                                            index % 2 === 0 ? "bg-gray-50" : "bg-white"
-                                        } hover:bg-gray-100 transition`}
+                                    <tr
+                                        key={index}
+                                        className={`${index === data.length - 1 ? '' : 'border-b-2 dark:border-b'
+                                            } hover:bg-gray-100 transition`}
                                     >
-                                        <td className="px-4 py-3 text-sm font-medium text-gray-800">{row.challenge}</td>
-                                        <td className="px-4 py-3 text-sm text-gray-600">{row.dates}</td>
-                                        <td className="px-4 py-3 text-sm text-gray-600">{row.amount}</td>
-                                        <td className="px-4 py-3 text-sm text-gray-600">{row.order}</td>
-                                        <td className="px-4 py-3 text-sm text-gray-600">{row.account}</td>
-                                        <td 
-                                            className={`px-4 py-3 text-sm font-semibold ${
-                                                row.status === "Pagado"
+                                        <td className="px-4 py-3 text-sm font-medium text-gray-800 dark:text-white">{row.challenge}</td>
+                                        <td className="px-4 py-3 text-sm text-gray-600 dark:text-white ">{row.dates}</td>
+                                        <td className="px-4 py-3 text-sm text-gray-600 dark:text-white">{row.amount}</td>
+                                        <td className="px-4 py-3 text-sm text-gray-600 dark:text-white">{row.order}</td>
+                                        <td className="px-4 py-3 text-sm text-gray-600 dark:text-white">{row.account}</td>
+                                        <td
+                                            className={`px-4 py-3 text-sm font-semibold ${row.status === "Pagado"
                                                     ? "text-green-600"
                                                     : row.status === "Pendiente"
-                                                    ? "text-yellow-600"
-                                                    : "text-red-600"
-                                            }`}
+                                                        ? "text-yellow-600"
+                                                        : "text-red-600"
+                                                }`}
                                         >
                                             {row.status}
                                         </td>
@@ -109,9 +106,9 @@ const Billing = () => {
                                         </td>
                                     </tr>
                                 ))
-                                
                             )}
                         </tbody>
+
                     </table>
                 </div>
             </div>
