013ee0f09da7643b0b4b232baa361a3b4b56a116
NeocapitalDev <neocapital2025@gmail.com>
2025-01-21 17:52:41 -0500

    metrix

--- src/pages/metrix/[idcuenta].js
+++ src/pages/metrix/[idcuenta].js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../../components/layout/dashboard';
-import { ChartBarIcon } from '@heroicons/react/24/outline'; 
+import { ArrowPathIcon, ChartBarIcon, PhoneIcon, UserIcon } from '@heroicons/react/24/outline';
 import MetrixDash from '../../components/structure/dashboard/metrixdash';
 import MetrixInfo from '../../components/structure/dashboard/metrixinfo';
 import Objetivos from '../../components/structure/dashboard/objetivos';
@@ -15,14 +15,22 @@ const Metrix = () => {
         <Layout title="Metrix">
             <div className="p-0 ">
                 <h1 className="bg-white rounded-md p-4 text-xl font-bold mb-4 flex items-center  dark:bg-zinc-800 border-gray-200  border-2  shadow-md dark:text-white dark:border-zinc-800 dark:shadow-black">
-                    <ChartBarIcon className="w-6 h-6 mr-2 text-gray-700 " />
+                    <ChartBarIcon className="w-6 h-6 mr-2 text-gray-700 dark:text-white " />
                     Account Metrix {idcuenta ? idcuenta : 'Cargando...'}
                 </h1>
                 <div className="flex justify-start gap-3 mb-4">
-                    <button className="p-6 dark:bg-zinc-800 border-gray-200  dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2  hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">Credenciales</button>
-                    <button className="p-6 dark:bg-zinc-800 border-gray-200  dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2  hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">Contacte con nosotros</button>
-                    <button className="p-6 dark:bg-zinc-800 border-gray-200  dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2  hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">Actualizar</button>
-
+                    <button className="flex items-center p-6 dark:bg-zinc-800 border-gray-200 dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2 hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">
+                        <UserIcon className="w-5 h-5 mr-2 text-zinc-800 dark:text-white" />
+                        Credenciales
+                    </button>
+                    <button className="flex items-center p-6 dark:bg-zinc-800 border-gray-200 dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2 hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">
+                        <PhoneIcon className="w-5 h-5 mr-2 text-zinc-800 dark:text-white" />
+                        Contacte con nosotros
+                    </button>
+                    <button className="flex items-center p-6 dark:bg-zinc-800 border-gray-200 dark:text-white dark:border-zinc-800 dark:shadow-black bg-white border-2 hover:border-slate-400 py-2 px-4 rounded-md shadow-md transition duration-300">
+                        <ArrowPathIcon className="w-5 h-5 mr-2 text-zinc-800 dark:text-white" />
+                        Actualizar
+                    </button>
                 </div>
 
                 <div className="flex space-x-4">
@@ -33,7 +41,7 @@ const Metrix = () => {
 
                     <div className="w-1/4">
                         <h2 className="text-lg font-semibold">Free Trial {idcuenta}</h2>
-                            <MetrixInfo />
+                        <MetrixInfo />
                     </div>
                 </div>
                 <h2 className="text-lg mt-5 font-semibold">Objetivos</h2>
