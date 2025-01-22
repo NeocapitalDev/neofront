18ae0acc46ea10bbef6e0d7e1e28cb1f6cde46cc
ronnaldrangel <neocapital2025@gmail.com>
2025-01-21 20:22:59 -0500

    siderbar ajuste

--- src/components/structure/sidebar.js
+++ src/components/structure/sidebar.js
@@ -3,16 +3,17 @@ import { useRouter } from 'next/router';
 import { navigation } from './links'; // Importar desde links.js
 import PrincipalButton from './principalButton';
 
-
 export default function Sidebar() {
     const router = useRouter();
 
     return (
-        <div className='dark:bg-zinc-800 rounded-xl border-2 dark:border-0 dark:border-zinc-700 dark:shadow-black border-gray-200 dark:text-white'>
+        <div
+            className="relative dark:bg-zinc-800  h-full w-64 rounded-xl border-2 dark:border-0 dark:border-zinc-700 dark:shadow-black border-gray-200 dark:text-white"
+            style={{ minWidth: '19rem' }} // Asegurar un ancho fijo
+        >
             <div className="p-4 border-b border-gray-200 dark:border-zinc-700">
                 {/* Aquí se utiliza el nuevo componente con los props correspondientes */}
                 <PrincipalButton />
-
 
                 <p className="text-black dark:text-white font-semibold text-lg mt-8">
                     Menú principal
@@ -25,7 +26,7 @@ export default function Sidebar() {
                         href={href}
                         key={id}
                         className={`w-full flex items-center py-7 px-6 font-semibold transition duration-200 last:rounded-b-md relative 
-             ${router.pathname === href
+            ${router.pathname === href
                                 ? 'bg-gradient-to-r from-amber-50 via-white to-transparent border-l-4 border-amber-500 dark:bg-gradient-to-r dark:from-amber-800 dark:via-zinc-800 dark:to-transparent dark:border-l-4 dark:border-amber-500'
                                 : 'hover:bg-zinc-300 dark:hover:bg-zinc-700'
                             }`}
@@ -54,8 +55,6 @@ export default function Sidebar() {
                             {name}
                         </p>
                     </Link>
-
-
                 ))}
             </div>
         </div>
