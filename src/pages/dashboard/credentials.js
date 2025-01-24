e9ccaaa271a8e11d449f5796b1e2b4b84493eac9
ronnaldrangel <neocapital2025@gmail.com>
2025-01-24 18:20:48 -0500

    ad

--- src/pages/dashboard/credentials.js
+++ src/pages/dashboard/credentials.js
@@ -2,20 +2,23 @@ import { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon, ClipboardDocumentIcon, PencilIcon, KeyIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
-export default function CredencialesModal({ login, password, server, platform }) {
+export default function CredencialesModal() {
     const [open, setOpen] = useState(false);
     const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar la contraseña
+    const [copied, setCopied] = useState({}); // Estado para el mensaje de "Copiado" para cada item
 
+    // Elimino la "Contraseña de solo lectura" del array de datos
     const data = [
-        { label: 'Login', value: login },
-        { label: 'Contraseña', value: password },
-        { label: 'Contraseña de solo lectura', value: 'YrI*9@IqHa9' },
-        { label: 'Servidor', value: server },
-        { label: 'Plataforma', value: platform },
+        { label: 'Login', value: '1420126402' },
+        { label: 'Contraseña', value: 'asAS*9@Pa9' },
+        { label: 'Servidor', value: 'FTMO-Demo2' },
+        { label: 'Plataforma', value: 'MT4' },
     ];
 
-    const handleCopy = (value) => {
+    const handleCopy = (label, value) => {
         navigator.clipboard.writeText(value); // Copia el valor al portapapeles sin alertas
+        setCopied(prev => ({ ...prev, [label]: true })); // Muestra el mensaje de copiado para el ítem
+        setTimeout(() => setCopied(prev => ({ ...prev, [label]: false })), 2000); // El mensaje desaparece después de 2 segundos
     };
 
     return (
@@ -28,7 +31,6 @@ export default function CredencialesModal({ login, password, server, platform })
                 <KeyIcon className="h-6 w-6 text-gray-600 dark:text-gray-200" />
                 <span className="text-xs lg:text-sm dark:text-zinc-200">Credenciales</span>
             </button>
-
 
             {/* Modal */}
             <Transition.Root show={open} as={Fragment}>
@@ -56,7 +58,7 @@ export default function CredencialesModal({ login, password, server, platform })
                                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                             >
-                                <Dialog.Panel className=" p-6  dark:bg-zinc-900 border-gray-200  border-2  dark:text-white dark:border-zinc-800 dark:shadow-black relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
+                                <Dialog.Panel className="p-6 dark:bg-zinc-900 border-gray-200 border-2 dark:text-white dark:border-zinc-800 dark:shadow-black relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
                                     <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                                         <button
                                             type="button"
@@ -68,8 +70,8 @@ export default function CredencialesModal({ login, password, server, platform })
                                         </button>
                                     </div>
 
-                                    <div className="mt-3 sm:mt-0 ">
-                                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 border-b pb-5 text-gray-900 text-left mb-4 dark:text-white ">
+                                    <div className="mt-3 sm:mt-0">
+                                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 border-b pb-5 text-gray-900 text-left mb-4 dark:text-white">
                                             Credenciales de inicio de sesión
                                         </Dialog.Title>
 
@@ -80,33 +82,21 @@ export default function CredencialesModal({ login, password, server, platform })
                                                 return (
                                                     <div
                                                         key={index}
-                                                        className={`flex  items-center ${!isLast ? 'justify-between' : ''} ${!isLast && 'border-b'} pb-2`}
+                                                        className={`flex items-center ${!isLast ? 'justify-between' : ''} ${!isLast && 'border-b'} pb-2`}
                                                     >
                                                         <div className="text-sm dark:text-white font-medium text-gray-900 flex items-center">
                                                             {item.label}:
-                                                            {['Contraseña', 'Contraseña de solo lectura'].includes(item.label) && (
-                                                                <svg
-                                                                    className="w-4 h-4 text-gray-600 ml-1"
-                                                                    viewBox="0 0 512 512"
-                                                                    xmlns="http://www.w3.org/2000/svg"
-                                                                >
-                                                                    <path
-                                                                        fill="#808080"
-                                                                        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
-                                                                    ></path>
-                                                                </svg>
-                                                            )}
                                                         </div>
 
-                                                        <div className={`flex  items-center py-1 ${!isLast ? 'space-x-20' : 'space-x-2'}`}>
+                                                        <div className={`flex items-center py-1 ${!isLast ? 'space-x-20' : 'space-x-2'}`}>
                                                             {/* Contraseña principal con opción de ocultar */}
                                                             {item.label === 'Contraseña' ? (
-                                                                <div className="flex  items-center space-x-1">
+                                                                <div className="flex items-center space-x-1">
                                                                     <span className="text-sm dark:text-white text-gray-600">
-                                                                        {showPassword ? item.value : '********'}
+                                                                        {showPassword ? item.value : '****'}
                                                                     </span>
                                                                     <button
-                                                                        className="flex   items-center justify-center"
+                                                                        className="flex items-center justify-center"
                                                                         onClick={() => setShowPassword(!showPassword)}
                                                                     >
                                                                         {showPassword ? (
@@ -118,7 +108,7 @@ export default function CredencialesModal({ login, password, server, platform })
                                                                 </div>
                                                             ) : (
                                                                 isLast ? (
-                                                                    <span className="text-md font-bold mx-2 dark:text-white  text-gray-900">{item.value}</span>
+                                                                    <span className="text-md font-bold mx-2 dark:text-white text-gray-900">{item.value}</span>
                                                                 ) : (
                                                                     <span className="text-sm dark:text-white text-gray-600">{item.value}</span>
                                                                 )
@@ -130,19 +120,22 @@ export default function CredencialesModal({ login, password, server, platform })
                                                                     <div className="flex items-center space-x-3">
                                                                         <button
                                                                             className="p-2 bg-white rounded border hover:bg-gray-200 w-12 h-12 flex items-center justify-center"
-                                                                            onClick={() => handleCopy(item.value)}
+                                                                            onClick={() => handleCopy(item.label, item.value)}
                                                                         >
                                                                             <ClipboardDocumentIcon className="h-5 w-5 text-gray-600" />
                                                                         </button>
-                                                                        <span className="text-sm text-gray-600 dark:text-white  truncate">Copiar</span>
+                                                                        <span className="text-sm text-gray-600 dark:text-white truncate">Copiar</span>
                                                                     </div>
 
-                                                                    {/* Botón de cambio */}
-                                                                    {['Contraseña', 'Contraseña de solo lectura'].includes(item.label) && (
+                                                                    {/* Mensaje de copiado */}
+                                                                    {copied[item.label] && (
+                                                                        <div className="text-sm text-green-500 mt-1">¡Copiado!</div>
+                                                                    )}
+
+                                                                    {/* Botón de cambio (solo para Contraseña) */}
+                                                                    {item.label === 'Contraseña' && (
                                                                         <div className="flex items-center space-x-3">
-                                                                            <button
-                                                                                className="p-2 border bg-white rounded hover:bg-gray-200 w-12 h-12 flex items-center justify-center"
-                                                                            >
+                                                                            <button className="p-2 border bg-white rounded hover:bg-gray-200 w-12 h-12 flex items-center justify-center">
                                                                                 <PencilIcon className="h-5 w-5 text-gray-600" />
                                                                             </button>
                                                                             <span className="text-sm text-gray-600 dark:text-white truncate">Cambio</span>
@@ -157,7 +150,7 @@ export default function CredencialesModal({ login, password, server, platform })
                                         </div>
                                     </div>
 
-                                    
+                              
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
