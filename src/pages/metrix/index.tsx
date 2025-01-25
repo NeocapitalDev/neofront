b5d4560f5023fe65db537ecae243051967d6884c
NeocapitalDev <neocapital2025@gmail.com>
2025-01-25 17:42:50 -0500

    dash

--- src/pages/metrix/index.tsx
+++ src/pages/metrix/index.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts"
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -17,11 +15,12 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart"
+
 const chartData = [
-  { trade: "0", balance: 0},
-  { trade: "1", balance: -123},
-  { trade: "2", balance: -78},
-  { trade: "2", balance: 40},
+  { trade: "0", balance: 0 },
+  { trade: "1", balance: -123 },
+  { trade: "2", balance: -78 },
+  { trade: "3", balance: 40 },
 ]
 
 const chartConfig = {
@@ -29,61 +28,65 @@ const chartConfig = {
     label: "Balance",
     color: "hsl(var(--chart-1))",
   },
-  // mobile: {
-  //   label: "Mobile",
-  //   color: "hsl(var(--chart-2))",
-  // },
 } satisfies ChartConfig
 
 export default function Component() {
   return (
     <div>
-          <Card>
-      <CardHeader>
-        <CardTitle className="font-normal text-black dark:text-white">Balance</CardTitle>
-        <CardDescription className="text-4xl font-semibold text-black dark:text-white">$99,921</CardDescription>
-
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <LineChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              left: 12,
-              right: 12,
-            }}
-          >
-            <CartesianGrid horizontal={true} strokeWidth={2} vertical={false} />
-            <XAxis
-              dataKey="trade"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Line
-              dataKey="balance"
-              type="natural"
-              stroke="#FFC107"
-              strokeWidth={2}
-              dot={{
-                fill: "#FFC107",
+      <Card>
+        <CardHeader>
+          <CardTitle className="font-normal text-black dark:text-white">
+            Balance
+          </CardTitle>
+          <CardDescription className="text-4xl font-semibold text-black dark:text-white">
+            $99,921
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
               }}
-              activeDot={{
-                r: 6,
-              }}
-            />
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
-
-    </Card>
+            >
+              <CartesianGrid horizontal={true} strokeWidth={2} vertical={false} />
+              <XAxis
+                dataKey="trade"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+              />
+              <YAxis
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                domain={[-150, 150]} // Ajusta manualmente para centrar el 0
+                tickCount={7} // Número de divisiones en el eje
+                tickFormatter={(value) => `$${value}`}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey="balance"
+                type="natural"
+                stroke="#FFC107"
+                strokeWidth={2}
+                dot={{
+                  fill: "#FFC107",
+                }}
+                activeDot={{
+                  r: 6,
+                }}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
     </div>
-
   )
 }
