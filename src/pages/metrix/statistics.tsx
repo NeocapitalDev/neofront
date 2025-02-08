fdbae746e9b5a7dae6cfecba81e254cc2665459f
NeocapitalDev <neocapital2025@gmail.com>
2025-02-08 10:19:44 -0500

    Resultados Actuales
    Estadísticas

---
+++ src/pages/metrix/statistics.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Component({ data }) {
+
+
+  return (
+    <>
+      <div className="   border-gray-200  border-2   dark:border-zinc-800 dark:shadow-black   p-3  bg-white rounded-md shadow-md dark:bg-zinc-800 dark:text-white ">
+              <p className="text-lg font-semibold mb-4">Resultados Actuales</p>
+
+       </div>
+    </>
+  );
+}
