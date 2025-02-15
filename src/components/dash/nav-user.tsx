3a248d21f17b5ecc199169b9a21440a1a2e8204b
NeocapitalDev <neocapital2025@gmail.com>
2025-02-15 17:09:44 -0500

    fix admin

--- src/components/dash/nav-user.tsx
+++ src/components/dash/nav-user.tsx
@@ -62,8 +62,7 @@ export function NavUser({
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name} </span>
               </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
+             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
