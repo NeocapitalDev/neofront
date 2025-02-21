ff7e6f33df12dec92ecfd9ab5d3e83f4beff0aa1
ronnaldrangel <neocapital2025@gmail.com>
2025-02-21 15:14:47 -0500

    .

--- src/components/structure/principalButton.js
+++ src/components/structure/principalButton.js
@@ -6,7 +6,7 @@ import { principalButton } from './links';
 const PrincipalButton = () => {
     return (
         <Link href={principalButton[0].href} passHref>
-            <button className="bg-[var(--app-secondary)] rounded-md text-black font-semibold px-4 py-3 w-full hover:bg-[var(--app-secondary)] transition duration-200">
+            <button className="bg-[var(--app-primary)] rounded-md text-black font-semibold px-4 py-3 w-full hover:bg-[var(--app-secondary)] transition duration-200">
                 {principalButton[0].name}
             </button>
         </Link>
