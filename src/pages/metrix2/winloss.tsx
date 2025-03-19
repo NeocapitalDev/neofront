070514f1b7e181f7d7042983c43a29f205c387ee
ivanfernandeze <ivanfer105esq@gmail.com>
2025-03-19 11:05:26 -0500

    barra winloss

--- src/pages/metrix2/winloss.tsx
+++ src/pages/metrix2/winloss.tsx
@@ -63,12 +63,12 @@ export default function WinLoss({ data }) {
   };
 
   return (
-    <div className="mt-6 w-full">
+    <div className="mt-6 w-full ">
       <h2 className="text-lg font-semibold flex items-center">
         <BarChart className="w-5 h-5 mr-2" /> 
         Win/Loss Rates
       </h2>
-      <Card className="mt-4">
+      <Card className="mt-4 dark:bg-zinc-800">
         <CardContent className="pt-6">
           {noTrades ? (
             <div className="text-center py-8 text-muted-foreground">
