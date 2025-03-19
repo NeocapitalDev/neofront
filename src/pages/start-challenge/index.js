40b5c96e805e4f593e0b605d93d75d92aad7b476
NarutoGM <t1053300121@unitru.edu.pe>
2025-03-19 17:12:30 -0500

    debug

--- src/pages/start-challenge/index.js
+++ src/pages/start-challenge/index.js
@@ -145,9 +145,10 @@ const ChallengeRelations = () => {
     process.env.NEXT_PUBLIC_WC_CONSUMER_KEY;
   const hasCredentials = !!consumerKey;
   const shouldFetch = endpoint && hasCredentials;
-  console.log(consumerKey)
-  console.log(hasCredentials)
-  console.log(shouldFetch)
+  
+  console.log("consumerKey",consumerKey)
+  console.log("hasCredentials",hasCredentials)
+  console.log("shouldFetch",shouldFetch)
 
   // Use useSWR unconditionally to fetch variations
   const {
